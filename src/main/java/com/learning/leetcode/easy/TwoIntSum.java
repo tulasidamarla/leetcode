5cbd3b1bef98980e6a8634d377122baf6a00919d
tulasidamarla <ram.damarla@gmail.com>
2023-06-12 18:31:39 +0530

    refactored notes

--- src/main/java/com/learning/leetcode/easy/TwoIntSum.java
+++ src/main/java/com/learning/leetcode/easy/TwoIntSum.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
-* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-* You may assume that each input would have exactly one solution, and you may not use the same element twice.
-* You can return the answer in any order.
-* */
+ * Given an array of integers and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * */
 public class TwoIntSum {
 
-    public static int[] twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> indexMap = new HashMap<>();
-        for(int i = 0; i< nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int current = nums[i];
             int required = target - current;
-            if (indexMap.containsKey(required)){
+            if (indexMap.containsKey(required)) {
                 return new int[]{i, indexMap.get(required)};
             }
             indexMap.put(current, i);
@@ -23,21 +23,22 @@ public class TwoIntSum {
         return null;
     }
 
-    public static void printTestResult(int[] nums, int[] result, int testCaseNumber, int target){
-        System.out.println(result[0] + " " + result[1]);
-        if (result[0] != result[1] ){
-            if(nums[result[0]] + nums[result[1]] == target) {
+    public static void printTestResult(int[] nums, int[] result, int testCaseNumber, int target) {
+
+        if (result != null && result[0] != result[1]) {
+            if (nums[result[0]] + nums[result[1]] == target) {
                 System.out.println("Test case " + testCaseNumber + " is successful ");
-            } else {
-                System.out.println("Test case " + testCaseNumber + " is failed");
+                System.out.println("Indices are: " + result[0] + " " + result[1]);
             }
+        } else {
+            System.out.println("Test case " + testCaseNumber + " is failed");
         }
     }
 
     public static void main(String[] args) {
 
         //Test case 1
-        int[] nums = {2,7,11,15};
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] result = twoSum(nums, target);
         printTestResult(nums, result, 1, target);
@@ -54,5 +55,10 @@ public class TwoIntSum {
         result = twoSum(nums, target);
         printTestResult(nums, result, 3, target);
 
+        //Test case 4
+        nums = new int[]{3, 3};
+        target = 5;
+        result = twoSum(nums, target);
+        printTestResult(nums, result, 4, target);
     }
 }
